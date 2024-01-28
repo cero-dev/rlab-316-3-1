e63645c97578d47430963e3d829a19c2744c6968
cero-dev <153134353+cero-dev@users.noreply.github.com>
2024-01-28 11:20:18 -0600

    completed part one, and two, first half lab

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
+// selecting the element with the 'main' tag
 const mainEl = document.querySelector("main");
-mainEl.classList.add('flex-ctr')
 
+// adding a class, and changing backgroundcolor to mainEl
+mainEl.classList.add('flex-ctr')
 mainEl.style.backgroundColor = 'var(--main-bg)';
 
+// creating new element and setting it's text content
 const mainH1 = document.createElement("h1");
 mainH1.textContent = "DOM Manipulation";
 
+// attatching the new element to mainEl
 mainEl.appendChild(mainH1);
+
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
+topMenuEl.classList.add("flex-around");
