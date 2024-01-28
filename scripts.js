635f08b2e25e823893cd6f628377cdd0b2421958
cero-dev <153134353+cero-dev@users.noreply.github.com>
2024-01-28 12:31:05 -0600

    completed part four, second half of lab

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
+// menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    {
+        text: 'catalog', href: '#', subLinks: [
+            { text: 'all', href: '/catalog/all' },
+            { text: 'top selling', href: '/catalog/top' },
+            { text: 'search', href: '/catalog/search' },
+        ]
+    },
+    {
+        text: 'orders', href: '#', subLinks: [
+            { text: 'new', href: '/orders/new' },
+            { text: 'pending', href: '/orders/pending' },
+            { text: 'history', href: '/orders/history' },
+        ]
+    },
+    {
+        text: 'account', href: '#', subLinks: [
+            { text: 'profile', href: '/account/profile' },
+            { text: 'sign out', href: '/account/signout' },
+        ]
+    },
+];
+
 // selecting the element with the 'main' tag
 const mainEl = document.querySelector("main");
 
@@ -12,7 +37,46 @@ mainH1.textContent = "DOM Manipulation";
 // attatching the new element to mainEl
 mainEl.appendChild(mainH1);
 
+// created element from the id #top-menu
 const topMenuEl = document.getElementById("top-menu");
+
+// setting the height, background color, and adding the class 'flex-around' to topMenuEl
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
 topMenuEl.classList.add("flex-around");
+
+let topMenuLinks = []
+
+// for each element in the array, create an anchor tag, set it's href to the href in the object
+// and the text content to the text in the object.
+menuLinks.forEach(element => {
+    const newAnchorElement = document.createElement("a")
+    newAnchorElement.setAttribute('href', element.href)
+    newAnchorElement.textContent = element.text;
+
+    topMenuEl.appendChild(newAnchorElement);
+    topMenuLinks.push(newAnchorElement);
+});
+
+const subMenuEl = document.getElementById("sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+topMenuEl.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (e.target.tagName === "A") {
+        topMenuLinks.forEach(element => {
+            if (element === e.target) {
+                element.classList.toggle("active");
+            } else {
+                element.classList.remove("active");
+            }
+        });
+    } else {
+        return;
+    }
+})
